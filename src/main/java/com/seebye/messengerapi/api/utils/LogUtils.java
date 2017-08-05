22cff925f756c6a3d7baf3d99a624d8c3230f701
lior2 <voiIce123>
2017-08-05 23:26:15 +0300

    modifications to compile

--- src/main/java/com/seebye/messengerapi/api/utils/LogUtils.java
+++ src/main/java/com/seebye/messengerapi/api/utils/LogUtils.java
@@ -6,6 +6,10 @@ import android.util.Log;
 
 import com.seebye.messengerapi.api.BuildConfig;
 
+//import com.example.examplemodul.BuildConfig;
+
+// import com.seebye.messengerapi.api.BuildConfig;
+
 /**
  * Created by Nico on 03.05.2015.
  */
@@ -13,7 +17,9 @@ public class LogUtils
 {
 	public static void i(String strText, Object... aArgs)
 	{
-		if(BuildConfig.DEBUG)
+		if(
+
+				BuildConfig.DEBUG)
 		{
 			Log.i("MAPIM", String.format(strText, aArgs));
 		}
